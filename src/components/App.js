463f7386c2d74f45b50e54dda9e68f2758dd2cff
Stas <leshuksu@gmail.com>
2022-11-25 08:45:06 +0300

    Started work with SongList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { selectSong } from "../actions";
+import SongList from "./SongList";
 
 const App = () => {
     return (
         <div>
-            <h1>Hello!</h1>
+            <SongList />
         </div>
     );
 };
