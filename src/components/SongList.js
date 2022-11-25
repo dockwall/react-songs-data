e6a703c7e538ad6c675ad3668c7e093160eba5ff
Stas <leshuksu@gmail.com>
2022-11-25 19:01:30 +0300

    Connected SongList component with Redux Store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 class SongList extends React.Component {
     render() {
         return (
             <div>
                 Song List
-            </div>)
+                {console.log(this.props)}
+            </div>
+        )
     }
 };
 
-export default SongList;
+// This function gets Redux state and returns information relevant to the given component
+// Name of this function is recomend by convention but not constant
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+};
+
+// We use 'connect' fn function for getting "partial snapshot" of Redux store to our component as a prop
+// And that's it! Now we can use this prop in our component
+export default connect(mapStateToProps)(SongList);
