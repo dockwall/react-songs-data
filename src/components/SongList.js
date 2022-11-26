92084b00c2b1e738437f116f9e9739d6aa50e106
Stas <leshuksu@gmail.com>
2022-11-26 18:49:39 +0300

    Completed Songs List

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,25 @@ import React from "react";
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+    renderList() {
+        return this.props.songs.map(song => {
+            return (
+                <div className='item' key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">Show info</button>
+                    </div>
+                    <div className="content">
+                        <h2>{song.title}</h2>
+                    </div>
+                </div>
+            );
+        })
+    }
+
     render() {
         return (
-            <div>
-                Song List
-                {console.log(this.props)}
+            <div className="ui divided list">
+                {this.renderList()}
             </div>
         )
     }
