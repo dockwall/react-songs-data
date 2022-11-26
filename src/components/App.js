858eceef4ea263f165fb36dd5f85635b8aa6ddaf
Stas <leshuksu@gmail.com>
2022-11-27 00:31:22 +0300

    Connected Action Creator as click handler to all songs

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { selectSong } from "../actions";
 import SongList from "./SongList";
 
 const App = () => {
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { selectSong } from '../actions'
 
 class SongList extends React.Component {
     renderList() {
@@ -7,7 +8,12 @@ class SongList extends React.Component {
             return (
                 <div className='item' key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Show info</button>
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
+                            Show info
+                        </button>
                     </div>
                     <div className="content">
                         <h2>{song.title}</h2>
@@ -29,9 +35,10 @@ class SongList extends React.Component {
 // This function gets Redux state and returns information relevant to the given component
 // Name of this function is recomend by convention but not constant
 const mapStateToProps = (state) => {
+    console.log(state)
     return { songs: state.songs };
 };
 
 // We use 'connect' fn function for getting "partial snapshot" of Redux store to our component as a prop
 // And that's it! Now we can use this prop in our component
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
