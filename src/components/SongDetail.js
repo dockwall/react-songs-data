adc52434b101e7376fd19d5dbba4a0b39c223ad1
Stas <leshuksu@gmail.com>
2022-11-28 07:39:17 +0300

    Updated SongDetail JSX

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -5,9 +5,18 @@ const SongDetail = ({ selectedSong }) => {
     return selectedSong ? (
         <div>
             <h2>Song information:</h2>
-            <p>{`${selectedSong.title}, ${selectedSong.duration}`}</p>
+            <p>
+                Title: {selectedSong.title}
+                <br />
+                Duration: {selectedSong.duration}
+            </p>
+
         </div>
-    ) : null;
+    ) : (
+        <div>
+            <h2>Select a song</h2>
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
